855c071740202b7fd51ae304bab4351bde2291cf
Abu Bakar Siddik <bakaralosiddik2001momin@gmail.com>
2026-04-15 09:56:53 +0600

    multer file updated

--- server/configs/multer.js
+++ server/configs/multer.js
@@ -1,10 +1,3 @@
 import multer from "multer";
 
-const storage = multer.diskStorage({
-  destination: "uploads/",
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname);
-  },
-});
-
-export const upload = multer({ storage });
+export const upload = multer({ storage: multer.diskStorage({}) });
